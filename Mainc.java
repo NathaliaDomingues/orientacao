85fcdd8fedacb1e4f206632d665fb64ddbb8d8a4
NATHALIA DOMINGUES FREITAS <146946721+NathaliaDomingues@users.noreply.github.com>
2025-07-31 16:19:06 -0300

    Update Mainc.java

--- Mainc.java
+++ Mainc.java
@@ -1,7 +1,11 @@
 package exercicio.alura;
+
+import java.util.Scanner;
+
 //3.CLASSE CLIMA
 public class Mainc {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Clima clima = new Clima();
 
         clima.temperatura = 30;
@@ -11,6 +15,9 @@ public class Mainc {
         System.out.println("A temperatura está: " + clima.temperatura + "°C");
         System.out.println("Umidade: " + clima.umidade + "%");
         System.out.println("Condicao: " + clima.condicao);
+        System.out.println("Escreva uma temperatura: ");
+        String temperaturaUsuario = scanner.nextLine();
+        System.out.println(temperaturaUsuario);
 
         clima.exibeFicha();
     }
