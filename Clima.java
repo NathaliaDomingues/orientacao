934ef19d72930c2d46025c6c65ab736b6336fd55
NATHALIA DOMINGUES FREITAS <146946721+NathaliaDomingues@users.noreply.github.com>
2025-07-31 16:18:45 -0300

    Update Clima.java

--- Clima.java
+++ Clima.java
@@ -4,12 +4,13 @@ public class Clima {
     int temperatura;
     int umidade;
     String condicao;
+    int temperaturaUsuario;
 
 void exibeFicha() {
-    if (temperatura >= 30) {
+    if (30 > temperaturaUsuario) {
         System.out.println("Está quente");
     } else {
         System.out.println("Está frio");
     }
-}
+    }
 }
